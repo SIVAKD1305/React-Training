a8036243c4b85b8b70f8b8eb96df0b39791d3a55
SIVAKD1305 <133256562+SIVAKD1305@users.noreply.github.com>
2024-10-19 13:10:15 +0530

    modified:   js/heart_trail.js

--- js/heart_trail.js
+++ js/heart_trail.js
@@ -1,15 +1,17 @@
 const body = document.querySelector("body");
-console.log(body)
 body.addEventListener("mousemove",
     (event) => {
         const xpos = event.offsetX;
         const ypos = event.offsetY;
         const spanTag = document.createElement("span");
+        const size = Math.random()*100;
         spanTag.style.left = xpos+"px";
         spanTag.style.top = ypos+"px";
+        spanTag.style.width = size+ "px";
+        spanTag.style.height = size+ "px";
+        spanTag.style.backgroundSize = size+"px";
         body.append(spanTag);
         setTimeout(()=>{
             spanTag.remove()
         }, 3000)
-        // body.appendChild(spanTag);
     });
